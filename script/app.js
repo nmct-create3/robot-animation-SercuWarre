c12154efddfbefb09cda0146749bd9c91ef4dc0f
warresercu <warre.sercu@student.howest.be>
2022-10-27 16:16:02 +0200

    klaar

---
+++ script/app.js
@@ -0,0 +1,52 @@
+let tl = gsap.timeline({
+  defaults: {
+    duration: 0.75,
+    ease: 'power1.inout',
+  },
+  repeat: -1,
+  yoyo: true,
+});
+tl.set('#Shadow', {
+  transformOrigin: '50% 100%',
+});
+
+tl.fromTo(
+  '#Robot',
+  {
+    y: 2.5,
+  },
+  {
+    y: -2.5,
+  }
+).to(
+  '#Shadow',
+  {
+    scale: 0.75,
+  },
+  '<'
+);
+document.querySelector('.js-fast').addEventListener('click', () => {
+  console.log('fast');
+  tl.timeScale(2);
+});
+document.querySelector('.js-normal').addEventListener('click', () => {
+  console.log('normal');
+  tl.timeScale(1);
+});
+document.querySelector('.js-slow').addEventListener('click', () => {
+  console.log('slow');
+  tl.timeScale(0.5);
+});
+
+document.querySelector('.js-pause').addEventListener('click', () => {
+  console.log('pause');
+  if (!tl.paused()) {
+    tl.pause();
+    document.querySelector('.c-play-icon--play').style.display = 'block';
+    document.querySelector('.c-play-icon--pause').style.display = 'none';
+  } else {
+    tl.play();
+    document.querySelector('.c-play-icon--pause').style.display = 'block';
+    document.querySelector('.c-play-icon--play').style.display = 'none';
+  }
+});
